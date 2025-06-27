59ae62a1e51584edd5f8a2dbad56c6ce940210d5
Dhruv Ranajit Choudhury <dchoudhu@tcd.ie>
2025-06-27 16:52:09 +0200

    add run.sh

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,1 +1,27 @@
-python $1/code/main.py $2 $3
+#!/bin/bash
+
+# Usage:
+# ./run.sh <input_smiles_csv> <output_npy> <output_csv> [output_json] 
+# output json is optional
+
+set -e
+
+SMILES_PATH=$1
+OUTPUT_NPY=$2
+OUTPUT_CSV=$3
+OUTPUT_JSON=$4
+
+PYTHON_SCRIPT="./code/main.py"
+
+if [ -z "$OUTPUT_JSON" ]; then
+  python "$PYTHON_SCRIPT" \
+    --smiles "$SMILES_PATH" \
+    --output "$OUTPUT_NPY" \
+    --csv "$OUTPUT_CSV"
+else
+  python "$PYTHON_SCRIPT" \
+    --smiles "$SMILES_PATH" \
+    --output "$OUTPUT_NPY" \
+    --csv "$OUTPUT_CSV" \
+    --json "$OUTPUT_JSON"
+fi
